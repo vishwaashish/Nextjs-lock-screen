29fe9bca72bdf77a0c176ea830937c81bbfb2b42
neetesh09 <neeteshkumar.v@somaiya.edu>
2023-06-12 23:19:52 +0530

    add double click

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 import Head from "next/head";
 import Image from "next/image";
 import { useCallback, useEffect, useMemo, useState } from "react";
@@ -18,17 +18,17 @@ import background8 from "../assets/images/8.jpg";
 import background9 from "../assets/images/9.jpg";
 import Clock from "../components/Clock";
 import styles from "../styles/Home.module.css";
+import blurImage from "../assets/images/liquid-cheese.svg";
 
-const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
+const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
   ssr: false,
 });
 function getRandomNumber(maxLength = 1) {
   const max = maxLength;
   const min = max - (max - 1);
-  return Math.floor(Math.random() * (max - min + 1))
+  return Math.floor(Math.random() * (max - min + 1));
 }
 export default function Home() {
-
   const allBackgroundImages = useMemo(
     () => [
       background1,
@@ -49,12 +49,12 @@ export default function Home() {
     []
   );
 
-  const imagesLength = allBackgroundImages?.length
+  const imagesLength = allBackgroundImages?.length;
 
   const [loaded, setLoaded] = useState(false);
   const [zoomIn, setZoomIn] = useState(false);
   const [scroll, setScroll] = useState(() => {
-    return getRandomNumber(imagesLength)
+    return getRandomNumber(imagesLength);
   });
 
   useEffect(() => {
@@ -73,7 +73,7 @@ export default function Home() {
   }, [allBackgroundImages.length]);
 
   const generate = useCallback(() => {
-    setLoaded(true)
+    setLoaded(true);
     setScroll(getRandomNumber(imagesLength));
   }, [imagesLength]);
 
@@ -89,12 +89,11 @@ export default function Home() {
   const toggleFullScreen = useCallback(() => {
     if (getFullscreenElement()) {
       document.exitFullscreen();
-      setZoomIn(false)
+      setZoomIn(false);
     } else {
       document.documentElement.requestFullscreen().then(() => {
-        setZoomIn(true)
-      })
-
+        setZoomIn(true);
+      });
     }
   }, [getFullscreenElement]);
 
@@ -102,90 +101,117 @@ export default function Home() {
     toggleFullScreen();
   }, [toggleFullScreen]);
 
-  // const handleDoubleClick = (e) => {
-  //   if (e.detail === 2) {
-  //     return handleFullScreen();
-  //   }
-  // };
-
+  const handleDoubleClick = (e) => {
+    console.log(e.detail, "click");
+    // if (e.detail === 2) {
+    return handleFullScreen();
+    // }
+  };
 
   const onLoadingComplete = useCallback((e) => {
     setLoaded((val) => {
-      if (val) return false
-      return val
-    })
-  }, [])
+      if (val) return false;
+      return val;
+    });
+  }, []);
 
+  return (
+    <>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: "#fff",
+        }}
+        outerStyle={{
+          border: "2px solid #fff",
+        }}
+        clickables={["a", "button"]}
+      />
+      <div className={styles.container}>
+        <Head>
+          <title>Lock Screen</title>
+          <meta name="description" content="Lock Screen" />
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-
-  return (<>
-    <AnimatedCursor
-      innerSize={8}
-      outerSize={35}
-      innerScale={1}
-      outerScale={2}
-      outerAlpha={0}
-      hasBlendMode={true}
-      innerStyle={{
-        backgroundColor: '#fff'
-      }}
-      outerStyle={{
-        border: '2px solid #fff'
-      }}
-      clickables={[
-        'a',
-        'button',
-      ]}
-    />
-    <div className={styles.container}>
-      <Head>
-        <title>Lock Screen</title>
-        <meta name="description" content="Lock Screen" />
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <div >
-        <div className="container">
-          <div className={styles.fullImage} style={{ position: 'relative' }}>
-            <ImageComponent source={allBackgroundImages[scroll] || background1} onLoadingComplete={onLoadingComplete} />
+        <div>
+          <div className="container" onDoubleClick={handleDoubleClick}>
+            <div className={styles.fullImage} style={{ position: "relative" }}>
+              {/* <ImageComponent
+                source={allBackgroundImages[scroll] || background1}
+                onLoadingComplete={onLoadingComplete}
+              /> */}
+              <Image
+                onLoadingComplete={onLoadingComplete}
+                src={allBackgroundImages[scroll].src || background1.src}
+                alt={"Background Image"}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="bottom"
+                priority
+                // quality={100}
+                className={styles.imageEffect}
+                placeholder="blur"
+                blurDataURL={blurImage.src}
+              />
+            </div>
+            <div className={styles.contentBoxShadow}>
+              <Clock />
+            </div>
           </div>
-          <div className={styles.contentBoxShadow}>
-            <Clock />
+          <div className={styles.footer}>
+            <a
+              href="https://github.com/vishwaashish"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Ashishkumar Vishwakarma
+            </a>
+            <a
+              component="button"
+              className={styles.onZoom}
+              onClick={handleFullScreen}
+            >
+              {`Zoom ${zoomIn ? "Out" : "In"}`}
+            </a>
+            <a
+              component="button"
+              className={loaded ? "hideDecoration" : ""}
+              onClick={() => {
+                !loaded && generate();
+              }}
+            >
+              {loaded ? "Loading" : "Generate"}
+            </a>
           </div>
-        </div>
-        <div className={styles.footer}>
-          <a href="https://github.com/vishwaashish" target="_blank" rel="noreferrer">
-            Ashishkumar Vishwakarma
-          </a>
-          <a component="button" className={styles.onZoom} onClick={handleFullScreen}>
-            {`Zoom ${zoomIn ? "Out" : "In"}`}
-          </a>
-          <a component="button" className={loaded ? "hideDecoration" : ""} onClick={() => { !loaded && generate() }}>
-            {loaded ? "Loading" : "Generate"}
-          </a>
         </div>
       </div>
-    </div>
-  </>
+    </>
   );
 }
 
-
 const ImageComponent = ({ source, onLoadingComplete }) => {
-  const newSrc = useMemo(() => source.src, [source.src])
-  return (<Image
-    onLoadingComplete={onLoadingComplete}
-    src={newSrc}
-    alt={"Background Image"}
-    layout="fill"
-    objectFit="cover"
-    objectPosition="bottom"
-    priority
-    quality={100}
-    className={styles.imageEffect}
-  />)
-}
-
+  return (
+    <Image
+      onLoadingComplete={onLoadingComplete}
+      src={source.src}
+      alt={"Background Image"}
+      layout="fill"
+      objectFit="cover"
+      objectPosition="bottom"
+      priority
+      quality={100}
+      className={styles.imageEffect}
+      placeholder="blur"
+      blurDataURL={blurImage.src}
+    />
+  );
+};
